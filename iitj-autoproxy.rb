2e0ea6eaeb2e629233a998d01ae5dc23d61298cc
XanderWatson <saahil_bhavsar@outlook.com>
2023-09-01 09:36:38 +0530

    Brew formula update for iitj-autoproxy version v0.1.2

--- iitj-autoproxy.rb
+++ iitj-autoproxy.rb
@@ -12,7 +12,7 @@ class IitjAutoproxy < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/XanderWatson/iitj-autoproxy/releases/download/v0.1.2/iitj-autoproxy_0.1.2_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "f580a38b77b7600894c3f72ce3f1934cd1f9e3e9f14492a838f034806281b186"
+      sha256 "1ce50e602b23c852537fcd84ba45e7d1b829daebe4709ea6d9ef8c3aab60d897"
 
       def install
         bin.install "bin/autoproxyd"
@@ -25,11 +25,20 @@ class IitjAutoproxy < Formula
         bin.install "scripts/darwin/login.sh" => "autoproxyd-login"
       end
     end
-  end
+    if Hardware::CPU.intel?
+      url "https://github.com/XanderWatson/iitj-autoproxy/releases/download/v0.1.2/iitj-autoproxy_0.1.2_darwin_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "133c7ab1e7a7c553b196b47cab03b6b607ce8156a4f679dd8c8f0b7cf0e7a8fe"
 
-  def caveats
-    <<~EOS
-      How to use this binary
-    EOS
+      def install
+        bin.install "bin/autoproxyd"
+        bin.install "bin/autoproxy"
+
+        (etc/"iitj-autoproxy").install "autoproxy.config"
+
+        bin.install "scripts/darwin/daemon-start.sh" => "autoproxyd-start"
+        bin.install "scripts/darwin/daemon-stop.sh" => "autoproxyd-stop"
+        bin.install "scripts/darwin/login.sh" => "autoproxyd-login"
+      end
+    end
   end
 end
